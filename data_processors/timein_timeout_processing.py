c2cbdac0316289bff123e939028eed0a4b4516fe
Abduswamad <alawi_abdul@outlook.com>
2024-05-18 14:36:24 +0300

    data pocessing is ready for siplicity

--- data_processors/timein_timeout_processing.py
+++ data_processors/timein_timeout_processing.py
@@ -5,8 +5,28 @@ class TimeinTimeoutDataPreprocessor:
     def __init__(self, data):
         self.df = pd.DataFrame(data)
 
-    def clean_data(self):
+    def clean_data(self, working_hours=8, timein_hour=8, timein_minute=0, timeout_hour=17, timeout_minute=0):
         self.df['timein'] = pd.to_datetime(self.df['timein'])
         self.df['timeout'] = pd.to_datetime(self.df['timeout'])
         self.df['duration'] = self.df['timeout'] - self.df['timein']
+        # Determine Working Hours
+        self.df['is_undertime'] = self.df['duration'].apply(lambda x: x.total_seconds() / 3600 < working_hours)
+        self.df['is_ontime'] = self.df['duration'].apply(lambda x: x.total_seconds() / 3600 == working_hours)
+        self.df['is_overtime'] = self.df['duration'].apply(lambda x: x.total_seconds() / 3600 > working_hours)
+        # Determine Time In
+        self.df['is_early_in'] = self.df['timein'].apply(
+            lambda x: x < pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timein_hour, minute=timein_minute))
+        self.df['is_ontime_in'] = self.df['timein'].apply(
+            lambda x: x == pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timein_hour, minute=timein_minute))
+        self.df['is_late_in'] = self.df['timein'].apply(
+            lambda x: x > pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timein_hour, minute=timein_minute))
+        # Determine Time Out
+        self.df['is_early_out'] = self.df['timeout'].apply(
+            lambda x: x < pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timeout_hour, minute=timeout_minute))
+        self.df['is_ontime_out'] = self.df['timeout'].apply(
+            lambda x: x == pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timeout_hour,
+                                        minute=timeout_minute))
+        self.df['is_late_out'] = self.df['timeout'].apply(
+            lambda x: x > pd.Timestamp(year=x.year, month=x.month, day=x.day, hour=timeout_hour, minute=timeout_minute))
+
         return self.df
--- main.py
+++ main.py
@@ -3,18 +3,69 @@ from data_processors.timein_timeout_processing import TimeinTimeoutDataPreproces
 
 def main():
     # Sample Data
-    timein_timeout_data = {
-        "first_name": ["ALi"],
-        "timein": ["2024-05-18 09:05:00"],
-        "timeout": ["2024-05-18 16:50:00"],
-        "timein_status": ["LATE"],
-        "timeout_status": ["EARLY OUT"]
-    }
+    timein_timeout_data = [
+        {"Id Number":"01","timein":"2023-01-02 08:47:00","timeout":"2023-01-02 09:00:00"},
+        {"Id Number":"01","timein":"2023-01-03 08:03:00","timeout":"2023-01-03 17:02:00"},
+        {"Id Number":"01","timein":"2023-01-04 09:39:00","timeout":"2023-01-04 17:48:00"},
+        {"Id Number":"01","timein":"2023-01-05 09:21:00","timeout":"2023-01-05 18:11:00"},
+        {"Id Number":"01","timein":"2023-01-06 08:23:00","timeout":"2023-01-06 16:29:00"},
+        {"Id Number":"02","timein":"2023-01-02 09:05:00","timeout":"2023-01-02 18:02:00"},
+        {"Id Number":"02","timein":"2023-01-03 08:45:00","timeout":"2023-01-03 17:41:00"},
+        {"Id Number":"02","timein":"2023-01-04 09:12:00","timeout":"2023-01-04 18:05:00"},
+        {"Id Number":"02","timein":"2023-01-05 08:34:00","timeout":"2023-01-05 17:10:00"},
+        {"Id Number":"02","timein":"2023-01-06 09:07:00","timeout":"2023-01-06 17:56:00"},
+        {"Id Number":"03","timein":"2023-01-02 08:21:00","timeout":"2023-01-02 16:59:00"},
+        {"Id Number":"03","timein":"2023-01-03 08:52:00","timeout":"2023-01-03 17:40:00"},
+        {"Id Number":"03","timein":"2023-01-04 08:39:00","timeout":"2023-01-04 17:42:00"},
+        {"Id Number":"03","timein":"2023-01-05 09:05:00","timeout":"2023-01-05 18:01:00"},
+        {"Id Number":"03","timein":"2023-01-06 08:32:00","timeout":"2023-01-06 16:59:00"},
+        {"Id Number":"04","timein":"2023-01-02 09:11:00","timeout":"2023-01-02 18:04:00"},
+        {"Id Number":"04","timein":"2023-01-03 09:01:00","timeout":"2023-01-03 18:00:00"},
+        {"Id Number":"04","timein":"2023-01-04 08:34:00","timeout":"2023-01-04 17:38:00"},
+        {"Id Number":"04","timein":"2023-01-05 08:53:00","timeout":"2023-01-05 17:51:00"},
+        {"Id Number":"04","timein":"2023-01-06 08:50:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"05","timein":"2023-01-02 09:22:00","timeout":"2023-01-02 18:15:00"},
+        {"Id Number":"05","timein":"2023-01-03 08:40:00","timeout":"2023-01-03 17:20:00"},
+        {"Id Number":"05","timein":"2023-01-04 09:19:00","timeout":"2023-01-04 18:02:00"},
+        {"Id Number":"05","timein":"2023-01-05 08:41:00","timeout":"2023-01-05 17:34:00"},
+        {"Id Number":"05","timein":"2023-01-06 09:04:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"06","timein":"2023-01-02 09:00:00","timeout":"2023-01-02 17:45:00"},
+        {"Id Number":"06","timein":"2023-01-03 08:30:00","timeout":"2023-01-03 17:20:00"},
+        {"Id Number":"06","timein":"2023-01-04 09:10:00","timeout":"2023-01-04 18:00:00"},
+        {"Id Number":"06","timein":"2023-01-05 08:45:00","timeout":"2023-01-05 17:35:00"},
+        {"Id Number":"06","timein":"2023-01-06 09:20:00","timeout":"2023-01-06 18:05:00"},
+        {"Id Number":"07","timein":"2023-01-02 08:30:00","timeout":"2023-01-02 17:10:00"},
+        {"Id Number":"07","timein":"2023-01-03 08:40:00","timeout":"2023-01-03 17:30:00"},
+        {"Id Number":"07","timein":"2023-01-04 09:00:00","timeout":"2023-01-04 18:00:00"},
+        {"Id Number":"07","timein":"2023-01-05 08:45:00","timeout":"2023-01-05 17:40:00"},
+        {"Id Number":"07","timein":"2023-01-06 09:10:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"08","timein":"2023-01-02 09:05:00","timeout":"2023-01-02 18:00:00"},
+        {"Id Number":"08","timein":"2023-01-03 08:45:00","timeout":"2023-01-03 17:35:00"},
+        {"Id Number":"08","timein":"2023-01-04 09:15:00","timeout":"2023-01-04 18:10:00"},
+        {"Id Number":"08","timein":"2023-01-05 08:35:00","timeout":"2023-01-05 17:20:00"},
+        {"Id Number":"08","timein":"2023-01-06 09:10:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"09","timein":"2023-01-02 08:50:00","timeout":"2023-01-02 17:40:00"},
+        {"Id Number":"09","timein":"2023-01-03 08:40:00","timeout":"2023-01-03 17:30:00"},
+        {"Id Number":"09","timein":"2023-01-04 09:00:00","timeout":"2023-01-04 17:55:00"},
+        {"Id Number":"09","timein":"2023-01-05 08:45:00","timeout":"2023-01-05 17:35:00"},
+        {"Id Number":"09","timein":"2023-01-06 09:10:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"10","timein":"2023-01-02 09:05:00","timeout":"2023-01-02 18:00:00"},
+        {"Id Number":"10","timein":"2023-01-03 08:45:00","timeout":"2023-01-03 17:35:00"},
+        {"Id Number":"10","timein":"2023-01-04 09:15:00","timeout":"2023-01-04 18:10:00"},
+        {"Id Number":"10","timein":"2023-01-05 08:35:00","timeout":"2023-01-05 17:20:00"},
+        {"Id Number":"10","timein":"2023-01-06 09:10:00","timeout":"2023-01-06 17:55:00"},
+        {"Id Number":"11","timein":"2023-01-02 09:05:00","timeout":"2023-01-02 18:00:00"},
+        {"Id Number":"11","timein":"2023-01-03 08:45:00","timeout":"2023-01-03 17:35:00"},
+        {"Id Number":"11","timein":"2023-01-04 09:15:00","timeout":"2023-01-04 18:10:00"},
+    ]
     # Data Preprocessing
     timein_timeout_preprocessor = TimeinTimeoutDataPreprocessor(timein_timeout_data)
     timein_timeout_data_frame = timein_timeout_preprocessor.clean_data()
-    print(timein_timeout_data_frame)
     print(timein_timeout_data_frame.to_json(orient='records', date_format='iso'))
 
 if __name__ == "__main__":
     main()
+
+
+
+
